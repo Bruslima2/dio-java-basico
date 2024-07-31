b33416c5f30b4bc00a87af8c73948e841f5cecca
bruslima2 <bruslima1@gmail.com>
2024-07-31 07:08:12 -0300

    novooo

--- meu-primeiro-projeto-java/java-anatomia-classes/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/MinhaClasse.java
@@ -2,7 +2,19 @@ public class MinhaClasse {
 
     public static void main(String[] args) {
 
-        System.out.print("ola turma ");
+        String primeiroNome = "Bruno";
+        String segundoNome = "Sanches";
+
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+
+
+
+    }
+
+    public static  String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
 
     }
 }
